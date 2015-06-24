4544b0293384bbb3ae1b78c1b1e76d55189051c0
bigchief45 <jaime_andres@unitec.edu>
2015-06-24 08:52:23 +0000

    Modify gem file

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -8,6 +8,9 @@ class Workout < ActiveRecord::Base
     has_many :workout_exercises, dependent: :destroy
     accepts_nested_attributes_for :workout_exercises, :reject_if => :all_blank, :allow_destroy => true
     
+    after_save :update_goal
+    after_update :update_goal
+    after_destroy :update_goal
     
     def next
         self.class.where("id > ?", id).first
@@ -16,5 +19,12 @@ class Workout < ActiveRecord::Base
     def previous
         self.class.where("id < ?", id).last
     end
+   
+   
+   protected
+   
+        def update_goal
+            
+        end
     
 end
